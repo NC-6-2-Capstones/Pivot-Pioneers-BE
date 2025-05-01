397a572b8010b4294d95e48d9c1c8e8d1257dda9
EdDiazGRS <eddiaz.grs@gmail.com>
2025-05-01 17:42:06 -0400

    adding authentication views(register_token, login_token, and user_profile and urls

--- roadmap_backend/roadmap/urls.py
+++ roadmap_backend/roadmap/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import login_view, register_view, logout_view 
+from .views import login_view, register_view, logout_view, login_token, register_token, user_profile, view_or_edit_profile
 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('profile', view_or_edit_profile, name='view_or_edit_profile'),
+    path('api/auth/login/', login_token, name='api_login'),
+    path('api/auth/register/', register_token, name='api_register'),
+    path('api/auth/me/', user_profile, name='user_profile'),
 ]
