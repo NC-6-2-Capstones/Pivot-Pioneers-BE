ec0f78e25a730a85581e98e95921fd912d1ecc3c
Ahilen Romero <ahileenromero@protonmail.com>
2025-05-01 14:38:13 -0700

    add view/edit path

--- roadmap_backend/roadmap/urls.py
+++ roadmap_backend/roadmap/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view, register_view, logout_view 
+from .views import login_view, register_view, logout_view, view_or_edit_profile 
 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', view_or_edit_profile, name='view_or_edit_profile'),
 ]
