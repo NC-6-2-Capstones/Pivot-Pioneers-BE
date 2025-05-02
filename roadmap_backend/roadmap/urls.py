ca3c0d8aac58d8f5b662b0ebd68fcdcf011a002a
EdDiazGRS <124111413+EdDiazGRS@users.noreply.github.com>
2025-05-02 16:03:09 -0400

    Update urls.py
    fixed main syntax errors

--- roadmap_backend/roadmap/urls.py
+++ roadmap_backend/roadmap/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 
 from .views import login_view, register_view, logout_view, login_token, register_token, user_profile, view_or_edit_profile
-main
 
 
 urlpatterns = [
@@ -13,5 +12,4 @@ urlpatterns = [
     path('api/auth/login/', login_token, name='api_login'),
     path('api/auth/register/', register_token, name='api_register'),
     path('api/auth/me/', user_profile, name='user_profile'),
- main
 ]
