25e0c28f3055ec3e559b0a4b4fa812f233d2a096
EdDiazGRS <eddiaz.grs@gmail.com>
2025-05-08 15:43:21 -0400

    assessment questions models, views and url paths as well as seed questions to database

--- roadmap_backend/roadmap/urls.py
+++ roadmap_backend/roadmap/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, login_view, register_view, logout_view, login_token, register_token, user_profile, view_or_edit_profile
+from .views import home_view, login_view, register_view, logout_view, login_token, register_token, user_profile, view_or_edit_profile, submit_assessment, get_personality_profile, get_assessment_questions
 
 
 
@@ -13,4 +13,7 @@ urlpatterns = [
     path('api/auth/login/', login_token, name='api_login'),
     path('api/auth/register/', register_token, name='api_register'),
     path('api/auth/me/', user_profile, name='user_profile'),
+    path('api/assessments/questions/', get_assessment_questions, name='assessment_questions'),
+    path('api/assessments/submit/', submit_assessment, name='submit_assessment'),
+    path('api/assessments/profile/', get_personality_profile, name='personality_profile'),
 ]
